495c412abaf7ebac82088a96f9482ff5fa59b8d5
Jaka <jakaasha@gmail.com>
2025-03-18 14:50:36 +0700

    finish ch10

--- ch10/Readme.md
+++ ch10/Readme.md
@@ -1,5 +1,6 @@
 new things learned from each project:
 * rServer
+    * exercise 1: Include the functionality of binary.go in your own RESTful server
     * setup rest in main
         * Init mux routing with `mux := http.NewServeMux()`
         * assign each func to handler initiation with `mux.Handle("/endpoint", http.HandlerFunc(endpFunc))`
@@ -24,3 +25,42 @@ new things learned from each project:
         * init db conn with `sql.Open("postgresql", "host=")`
         * fetch data from db = `sql.Query` -> iterate `rows.Next` -> assign `rows.Scan(&var1, &var2, ...)` to var
         * mutate data to db = `stmt := sql.Prepare("UPDATE FROM table WHERE ID = $1")` -> `stmt.Exec(id)`
+
+* binary
+    * using `gorilla/mux` for routing with regex pattern matching
+    * static files served with `http.FileServer`
+    * binary file is brought along with `http.Request` body
+
+* swagger
+    * swagger generates docs based on comment in file
+    * docs format for route handler:
+    ```
+    // swagger:route [HTTP_METHOD] [PATH] [TAG] [OPERATION_ID]
+    // [Description of what the endpoint does]
+    //
+    // responses:
+    //   [STATUS_CODE]: [RESPONSE_TYPE]
+    //   [STATUS_CODE]: [RESPONSE_TYPE]
+    ```
+    * swagger models are defined with `// swagger:model` comment
+    * swagger parameters are defined with `// swagger:parameters [OPERATION_ID]`
+    * swagger responses are defined with `// swagger:response [RESPONSE_TYPE]`
+    * in-line documentation for struct fields with `// [Description]` and annotations like `// in: body`, `// required: true`, etc.
+    * `in: body` annotation indicates that the data will be sent in the JSON body of the HTTP request or returned in the response body.
+    * swagger comment can refer to existing entity (`// 404: ErrorMessage`)
+    * generating swagger documentation:
+        * install required tools:
+            * `go install github.com/go-swagger/go-swagger/cmd/swagger@latest` for spec generation
+            * `brew install swagger-codegen` for HTML docs generation
+        * generate swagger spec with `swagger generate spec -o ./swagger.yaml`
+        * generate HTML docs with `swagger-codegen generate -i swagger.yaml -l html2 -o docs`
+        * output will be in `docs/index.html` which can be viewed in browser
+    * serving swagger docs in Go application:
+        * use the `go-openapi/runtime/middleware` package
+        * set up a GET subrouter: `getMux := mux.Methods(http.MethodGet).Subrouter()`
+        * configure Redoc options: `opts := middleware.RedocOpts{SpecURL: "/swagger.yaml"}`
+        * create handler: `sh := middleware.Redoc(opts, nil)`
+        * register handlers:
+            * `getMux.Handle("/docs", sh)` - serves the Redoc UI at /docs endpoint
+            * `getMux.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))`- serves the swagger.yaml file
+        * access the documentation UI at http://localhost:PORT/docs
--- ch10/rServer/main.go
+++ ch10/rServer/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 type User struct {
@@ -16,10 +19,9 @@ type User struct {
 }
 
 var user User
-
 var PORT = ":1234"
-
 var DATA = make(map[string]string)
+var IMAGESPATH = "/tmp/files"
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Serving:", r.URL.Path, "From", r.Host)
@@ -143,30 +145,128 @@ func deleteHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("After:", DATA)
 }
 
+func saveToFile(path string, contents io.Reader) error {
+	_, err := os.Stat(path)
+	if err == nil {
+		err = os.Remove(path)
+		if err != nil {
+			log.Println("Error deleting", path)
+			return err
+		}
+	} else if !os.IsNotExist(err) {
+		log.Println("Unexpected error:", err)
+		return err
+	}
+
+	// If everything is OK, create the file
+	f, err := os.Create(path)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer f.Close()
+
+	// Write data to disk
+	n, err := io.Copy(f, contents)
+	if err != nil {
+		return err
+	}
+	log.Println("Bytes written:", n)
+
+	return nil
+}
+
+func createImageDirectory(d string) error {
+	_, err := os.Stat(d)
+	if os.IsNotExist(err) {
+		log.Println("Creating:", d)
+		err = os.MkdirAll(d, 0755)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	} else if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	fileInfo, _ := os.Stat(d)
+
+	mode := fileInfo.Mode()
+	if !mode.IsDir() {
+		msg := d + " is not a directory!"
+		return errors.New(msg)
+	}
+
+	return nil
+}
+
+// exercise 1: Include the functionality of binary.go in your own RESTful server
+func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	filename, ok := mux.Vars(r)["filename"]
+	if !ok {
+		log.Println("filename value not set!")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	log.Println("Uploading file:", filename)
+	err := saveToFile(IMAGESPATH+"/"+filename, r.Body)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Failed to save file", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "File %s uploaded successfully\n", filename)
+}
+
 func main() {
+	// Create uploads directory
+	err := createImageDirectory(IMAGESPATH)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 	arguments := os.Args
 	if len(arguments) != 1 {
 		PORT = ":" + arguments[1]
 	}
 
-	mux := http.NewServeMux()
+	router := mux.NewRouter()
 	s := &http.Server{
 		Addr:         PORT,
-		Handler:      mux,
+		Handler:      router,
 		IdleTimeout:  10 * time.Second,
 		ReadTimeout:  time.Second,
 		WriteTimeout: time.Second,
 	}
 
-	// assign func to handler init
-	mux.Handle("/time", http.HandlerFunc(timeHandler))
-	mux.Handle("/add", http.HandlerFunc(addHandler))
-	mux.Handle("/get", http.HandlerFunc(getHandler))
-	mux.Handle("/delete", http.HandlerFunc(deleteHandler))
-	mux.Handle("/", http.HandlerFunc(defaultHandler))
+	// File upload endpoints
+	putRouter := router.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/files/{filename:[a-zA-Z0-9][a-zA-Z0-9\\.]*[a-zA-Z0-9]}", uploadFileHandler)
+
+	// Serve uploaded files
+	getRouter := router.Methods(http.MethodGet).Subrouter()
+	fileServer := http.FileServer(http.Dir(IMAGESPATH))
+	getRouter.PathPrefix("/files/").Handler(http.StripPrefix("/files/", fileServer))
+
+	// Existing endpoints
+	router.HandleFunc("/time", timeHandler)
+	router.HandleFunc("/add", addHandler)
+	router.HandleFunc("/get", getHandler)
+	router.HandleFunc("/delete", deleteHandler)
+	router.HandleFunc("/", defaultHandler)
 
 	fmt.Println("Ready to serve at", PORT)
-	err := s.ListenAndServe()
+	err = s.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
 		return
